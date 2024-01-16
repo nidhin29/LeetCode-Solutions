2a6e722d26c26e518e79b57ebeb7f8caf46b1a6a
Nidhin Ninan <nidhinvninan29@gmail.com>
2024-01-16 22:47:39 +0530

    modified

--- Find Players With Zero or One Losses.java
+++ Find Players With Zero or One Losses.java
@@ -2,93 +2,37 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
-    public List<List<Integer>> findWinners(int[][] matches) {
-        int length = matches.length;
-        int winCount =0;
-        List<Integer> con1 = new ArrayList<>();
-        List<Integer> con2 = new ArrayList<>();
-        List<Integer> con3 = new ArrayList<>();
-        int losecount = 0;
+  public List<List<Integer>> findWinners(int[][] matches) {
+    Set<Integer> winners = new HashSet<>();
+    Map<Integer, Integer> losers = new HashMap<>();
 
-        for (int i = 0; i < length; i++) {
-            int winner = matches[i][0];
-            for (int j = 0; j < length; j++) 
-            {  
-                if (winner != matches[j][1])
-                 {
-                   winCount++;
-                 } 
-                else 
-                {
-                  losecount++;
-                }
-            }
-           if(winCount== (length)&& losecount == 0)
-           {
-              con1.add(winner);
-           }
-           else if(losecount == 1 && winCount>0)
-           {
-              con2.add(winner);
-           }
-           winCount=0;
-           losecount=0;
-        }
-         for (int i = 0; i < length; i++) 
-         {
-             con3.add(matches[i][1]);
-         }
-
-
-        int len2 = con3.size();
-        int len = con1.size();
-        int len1 = con2.size();
-        List<List<Integer>> corr = new ArrayList<>();
-        List<Integer> h = new ArrayList<>();
-        List<Integer> l = new ArrayList<>();
-         List<Integer> x = new ArrayList<>();
-         con1.sort(null);
-         con2.sort(null);
-        for (int i = 0; i < len; i++)
-         {
-            if (!h.contains(con1.get(i))) {
-                h.add(con1.get(i));
-            }
-        }
-
-        for (int i = 0; i < len1; i++) {
-            if (!l.contains(con2.get(i))) {
-                l.add(con2.get(i));
-            }
-        } 
-       
-         for (int i = 0; i < len2; i++)
-         {  int c=0;
-            int num = con3.get(i);
-            for (int j=0;j<len2;j++)
-            {
-                 if(num == con3.get(j))
-                 {
-                      c++;
-                 }
-
-            }
-            if(c==1)
-            {
-                l.add(con3.get(i));
-            }
-         }
-           int len3 = l.size();
-          for (int i = 0; i < len3; i++) {
-            if (!x.contains(l.get(i))) {
-                x.add(l.get(i));
-            }
-        } 
-        x.sort(null);
-        corr.add(h);
-        corr.add(x);
-        return corr;
+    for (int[] match : matches) {
+        winners.add(match[0]);
+        losers.put(match[1], losers.getOrDefault(match[1], 0) + 1);
     }
+
+    List<Integer> noLosses = new ArrayList<>();
+    List<Integer> oneLoss = new ArrayList<>();
+
+    for (int winner : winners) {
+        if (!losers.containsKey(winner)) {
+            noLosses.add(winner);
+        } else if (losers.get(winner) == 1) {
+            oneLoss.add(winner);
+        }
+    }
+
+    for (Map.Entry<Integer, Integer> entry : losers.entrySet()) {
+        if (entry.getValue() == 1 && !winners.contains(entry.getKey())) {
+            oneLoss.add(entry.getKey());
+        }
+    }
+
+    Collections.sort(noLosses);
+    Collections.sort(oneLoss);
+
+    return Arrays.asList(noLosses, oneLoss);
+}
 }
 
 class Main 
