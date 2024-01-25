2bd2f25b25c3b65ad644e94c7dd039613a7d3020
Nidhin Ninan <nidhinvninan29@gmail.com>
2024-01-26 02:23:59 +0530

    modified

--- Count Primes.java
+++ Count Primes.java
@@ -4,22 +4,33 @@ class Solution {
             return 0;
         }
         boolean[] a = new boolean[n];
-        for(int i = 2; i<Math.sqrt(n); i++){
-            if(a[i]==false){
-                for(int j = i*i; j<n; j+=i){
-                    a[j]=true;
+        for(int i=2;i<n;i++)
+        {
+            a[i] = false;
+        }
+        for(int i=2;i*i<n;i++)
+        {
+            for(int j=i*i;j<n;j=j+i)
+            {
+                if(a[j]== false)
+                {
+                    a[j]= true;
                 }
             }
         }
-        int count = 0;
-        for(int i = 2; i<n; i++){
-            if(a[i]==false){
-                count++;
-            }
+        int count=0;
+        for(int i=2;i<n;i++)
+        {
+           if(a[i]== false)
+                {
+                    count++;
+                } 
         }
+
         return count;
     }
 }
+
 class Main
 {
     public static void main(String args[])
